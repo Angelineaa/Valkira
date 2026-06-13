41fd2a8dbde7f3ba860e89db8b0c792ab0bd4131
Angelineaa <angiearb1008@gmail.com>
2026-06-13 18:40:44 -0500

    Actualizar CORS 3

--- backend/src/main/java/com/modaai/backend/WebConfig.java
+++ backend/src/main/java/com/modaai/backend/WebConfig.java
@@ -1,4 +1,4 @@
-package com.modaai.backend.controllers;
+package com.modaai.backend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
